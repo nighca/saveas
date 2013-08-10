065058497abd743dd250887a5637f022ccd57bec
nighca <nighca@live.cn>
2013-08-10 19:10:48 +0800

    add qrcode mode & v2.3.0

--- extension/assets/js/popup.js
+++ extension/assets/js/popup.js
@@ -1,6 +1,7 @@
-var src_list = document.getElementById("src_list");
+var srcList = document.getElementById("src_list");
 var infoWrapper = document.getElementById("info");
 var searchIn = document.getElementById("search_in");
+var qrChecker = document.getElementById("qr_checker");
 
 var showInfo = function(info, time){
 	time = time || 2000;
@@ -8,6 +9,10 @@ var showInfo = function(info, time){
 	setTimeout(function(){
 		infoWrapper.innerHTML  ='';
 	}, time);
+};
+
+var getQR = function(item){
+	return item.newUrl + '?qrcode';
 };
 
 var getThumb = function(item){
@@ -26,29 +31,37 @@ var getThumb = function(item){
 
 function createNode(item){
 	var removeIcon = '<span class="remove_op">x</span>';
-	var img = '<img src="' + 
+	var qrcode = 
+		'<a href="' + getQR(item) + '" target="_blank" >' + 
+		'<img class="qrcode" src="' + 
+		getQR(item) + 
+		'">' + 
+		'</a>';
+	var thumb = 
+		'<img class="thumb" src="' + 
 		getThumb(item) + 
 		'" width="50">';
 	var link = '<a href="' + item.newUrl + '" target="_blank" >' + 
-		img +
+		thumb +
 		item.name + 
 		'</a>';
 
 	var node = '<li class="resource">' + 
 		removeIcon + 
 		link +
+		qrcode +
 		'</li>';
 
 	return node;
 }
 
 var renderResourceList = function(resources){
-	src_list.innerHTML = '';
+	srcList.innerHTML = '';
 	for(var i=0;i<resources.length;i++){
-		src_list.innerHTML = createNode(resources[i]) + src_list.innerHTML;
+		srcList.innerHTML = createNode(resources[i]) + srcList.innerHTML;
 	}
 
-	var resources = src_list.getElementsByClassName("resource");
+	var resources = srcList.getElementsByClassName("resource");
 
 	for (var i = resources.length - 1; i >= 0; i--) {
 		var remove = resources[i].getElementsByClassName("remove_op")[0];
@@ -98,4 +111,13 @@ search_in.addEventListener('keyup', function(e){
 	getResources(false, keyword);
 });
 
+qrChecker.addEventListener('click', function(e){
+	if(this.className === 'qr-off'){
+		this.className = 'qr-on';
+		srcList.className = 'qr';
+	}else{
+		this.className = 'qr-off';
+		srcList.className = '';
+	}
+});
 
